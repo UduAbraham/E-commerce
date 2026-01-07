6d4b0612bbfd7624dbbe5be7dcfaa558cdbb7a65
unknown <uduabraham12@gmail.com>
2026-01-07 21:04:48 +0100

    update: checkout

--- src/pages/CheckoutPage/index.tsx
+++ src/pages/CheckoutPage/index.tsx
@@ -40,8 +40,8 @@ export default function CheckoutPage() {
   ];
 
   const subtotal = cartItems.reduce((sum, item) => sum + item.price, 0);
-  const shipping = 40;
-  const taxes = 492.14;
+  const shipping = 60;
+  const taxes = 492;
   const total = subtotal + shipping + taxes;
 
   return (
